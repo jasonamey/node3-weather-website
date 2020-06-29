9c13a9824a74f0b3c93d5f901aeeb08102f1fd49
Jason Amey <jasonamey@Jasons-Air-2.fios-router.home>
2020-06-29 17:22:17 -0400

    name change

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Jason Amey'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Jason Amey'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         title : 'Help',
         helpText: 'This is some helpful text.',
-        name : "Andrew Mead"
+        name : "Jason Amey"
     })
 })
 
