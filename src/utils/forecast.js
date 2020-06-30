653fbaaefc20ce363a6ad5680f2f80f17e831855
Jason Amey <jasonamey@Jasons-Air-2.fios-router.home>
2020-06-30 07:28:29 -0400

    extended forecast changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,13 +4,17 @@ const forecast = (longitude, latitude, callback) => {
     const key = 'bf2e1df62494076b7645ca6e1496ec72'
     const url = `https://api.darksky.net/forecast/${key}/${longitude},${latitude}`
     request({url, json : true}, (error, { body }) => {
+        
         if(error){
             callback("No connection made", undefined)
         }
         else if(body.error){
             callback("Location not found", undefined)
         }else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            console.log(body.daily)
+            // callback(undefined, body.daily.summary + " " + body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, `${body.daily.summary} ${body.daily.data[0].summary} It is currently ${body.currently.temperature} degress out. There is a ${body.currently.precipProbability}% chance of rain.`)
+
         }
     })
 
